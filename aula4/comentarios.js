11629b4fed247a6d39c5ff7719f8ad96e84afa01
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-09 10:44:02 -0300

    aula6

---
+++ aula4/comentarios.js
@@ -0,0 +1,15 @@
+// Escrever um comentário
+console.log('Olá mundo'); //Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+
+/*
+Um bloco
+de comentário
+
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+// Vai ser ignorado
---
+++ aula6/index.js
@@ -0,0 +1,26 @@
+/*let nome = 'Samuel';
+// var é um forma antiga de declarar variáveis
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome ,'se chama Eduardo.');
+*/
+
+
+/*
+let nome; //Declarou a variável
+nome = 'Qualquer valor'; //Inicializando a variável
+console.log(nome);
+nome = 'Samuel';
+console.log(nome);
+*/
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços. Utilizamos camelCase
+//Case-sensitive
+// não podemos redeclarar as variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
