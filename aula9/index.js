44e3bf6e14d0bf07e38c6149d0f18e19bbc7dc2b
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-18 18:10:48 -0300

    aula9

---
+++ aula9/index.js
@@ -0,0 +1,12 @@
+// ECMAScript 2015 (ES6) JavaScript
+var nome = 'Samuel';
+var nome = 'Henrique';
+
+sobrenome = 'Gomes'; // NÂO FAÇA ISSO
+console.log(nome);
+console.log(sobrenome);
+
+let nome1 = 'Samuel';
+// let nome1 = 'Samuel'; não é possível fazer isso, redeclarar a variável com let
+
+console.log(nome1);
