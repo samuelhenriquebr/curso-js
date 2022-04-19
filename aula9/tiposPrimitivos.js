29bd8f4e386bc6263447b696bedd9fe8ad9e6ca7
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-18 21:32:46 -0300

    aula 9

---
+++ aula9/tiposPrimitivos.js
@@ -0,0 +1,11 @@
+// String, number, undefined, null, boolean, symbol
+const nome = 'Samuel'; // String
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomealuno; // undefined = não aponta para local nenhum na memória
+const sobrenomealuno = null; // nulo = não aponta para local nenhum na memória
+const boolean = false; // booleano = true, false (lógico)
+const aprovado = true;
+
+console.log(typeof(sobrenomealuno), sobrenomealuno);
+
