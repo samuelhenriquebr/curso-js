10054173daf0f69d010cec3d223266ac04bad858
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-19 07:50:35 -0300

    aula 10

---
+++ aula10/index.js
@@ -0,0 +1,29 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação (- / *)
+ * ** Potenciação
+ * % Resto da divisão
+ */
+/**
+ * Precedência de operações aritméticas
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+/**
+ * Incremento = ++
+ * Decremento = --
+ * 
+ * Operadores de Atribuição
+ * +=
+ * -=
+ * *=
+ * /=
+ */
+// NaN - Not a number
+// parseInt (Inteiro), parseFloat (decimais)
+const num1 = 10;
+const num2 = Number('5');
+console.log(num1 + num2);
+console.log(typeof(num2));
