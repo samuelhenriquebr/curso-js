1e7eeda3db2e27b7e5647bbb86b4ba5668de77d1
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-20 07:51:32 -0300

    aula 14

---
+++ aula14/index.js
@@ -0,0 +1,20 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 = ((num1* 100) +  (num2 * 100)) / 100;
+num1 = ((num1* 100) +  (num2 * 100)) / 100;
+num1 = ((num1* 100) +  (num2 * 100)) / 100;
+
+//num1 = Number(num1.toFixed(2));
+//num1 = parseFloat(num1.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+//console.log(num1.toString() + num2);
+//console.log(num1.toString(2));
+//console.log(num1);
+//console.log(num1.toFixed(4));
+//console.log(Number.isInteger(num1));
+//let temp = num1 + '5';
+//console.log(Number.isNaN(temp));
+//console.log(temp);
