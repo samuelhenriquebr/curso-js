248f227fd9d518b1b63474095a7ded0fd32ccc53
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-25 20:27:25 -0300

    aula 16

---
+++ aula16/index.js
@@ -0,0 +1,32 @@
+//            012345678910  
+const nome = 'Luiz Otávio';
+///               0        1        2    3    4     5  
+const alunos = ['Luiz', 'Maria', 'João', 1, true, null];
+alunos.push('Samuel');
+alunos.push('Henrique');
+
+console.log(typeof alunos);
+console.log( alunos instanceof Array);
+// console.log(alunos.slice(0, -2)); // Fatiamento
+
+// delete alunos[1];
+// const removido = alunos.shift(); // Remove no início
+// const removidoFinal = alunos.pop(); // Remove no final
+// console.log(removido)
+
+// console.log(alunos[50]); // Out fo boundary
+
+// alunos.unshift('Samuel'); //Adiciona no começo do array
+// alunos.unshift('Henrique');
+/* alunos.push('Samuel'); // Adiciona no fim
+alunos.push('Henrique'); */
+// alunos[alunos.length] = 'Samuel';
+// alunos[alunos.length] = 'Henrique';
+// alunos[0] = 'Samuel';
+// alunos[2] = 'Samuel';
+// alunos[6] = 'Samuel';
+//console.log(alunos[0]);
+//console.log(alunos);
+//console.log(alunos[2]);
+
+
