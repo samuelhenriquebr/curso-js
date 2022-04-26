4e0959a621834fa8ded1c6c0869272c204b7e75b
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-26 19:14:38 -0300

    aula 22

---
+++ aula22/index.js
@@ -0,0 +1,19 @@
+/* 
+Operadores Lógicos
+&& - > AND -> E -> Todas as expresões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÂO
+*/
+/* const expressaoAnd = true && false && true;
+const expressaoOr = true || false; 
+console.log(expressaoAnd);
+console.log(expressaoOr); */
+
+
+/* const usuario = 'Samuel';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Samuel' && senha === '12346';
+console.log(vaiLogar); */
+
+console.log(!true);
