96f1fce0fede71a3d7dc2f8d83f4ba0cb38d0cd1
Samuel Gomes <samuelvicosa@gmail.com>
2022-04-26 20:27:57 -0300

    aula23

---
+++ aula23/index.js
@@ -0,0 +1,30 @@
+/* 
+FALSY
+*false
+0
+'' "" `` strings vazias
+null / undefined
+NaN
+*/
+// console.log('Samuel' && undefined && 'Maria');
+
+/* function falaOi () {
+    return 'Oi';
+}
+
+let vaiExecutar = 'João';
+
+console.log(vaiExecutar && falaOi()); */
+
+/* const corUsuario = 'vermelho';
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao); */
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);
